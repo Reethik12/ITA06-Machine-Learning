933c46386a8d97cd6af11de893bc55ac9dda8032
Reethik12 <192473006.simats@saveetha.com>
2025-12-02 13:04:58 +0530

    Update EXP_2 Candidate Elimination Algorithm.py
    Refactored the Candidate Elimination Algorithm to read from 'enjoysport.csv' and updated the logic for generalizing and specializing hypotheses.

--- EXP_2 Candidate Elimination Algorithm.py
+++ EXP_2 Candidate Elimination Algorithm.py
@@ -1,47 +1,65 @@
 import csv
 
+file = open("enjoysport.csv", "r")
+csv_reader = csv.reader(file)
 
-with open("C:/Users/chait/Desktop/training.csv", "r") as f:
-    reader = csv.reader(f)
-    data = list(reader)
+dataset = []
+for row in csv_reader:
+    dataset.append(row)
 
+file.close()   
 
-num_attributes = len(data[0]) - 1
-S = ["Ø"] * num_attributes
-G = [["?"] * num_attributes]
+concepts = [row[:-1] for row in dataset]   
+target = [row[-1] for row in dataset]      
 
+print("Concepts:", concepts)
+print("Target:", target)
 
-for row in data:
-    attributes = row[:-1]
-    outcome = row[-1]
+def candidate_elimination(concepts, target):
 
-    if outcome == "Yes":
-       
-        for i in range(num_attributes):
-            if S[i] == "Ø":
-                S[i] = attributes[i]
-            elif S[i] != attributes[i]:
-                S[i] = "?"
+    specific_h = None
+    for i in range(len(target)):
+        if target[i] == "yes":
+            specific_h = concepts[i].copy()
+            break
 
-       
-        G = [g for g in G if all(s == "?" or s == g[i] for i, s in enumerate(S))]
+    general_h = [["?" for _ in specific_h]]
 
-    else:
-        
-        new_G = []
-        for g in G:
-            for i in range(num_attributes):
-                if S[i] != "?" and S[i] != attributes[i]:
-                    new_hypothesis = g.copy()
-                    new_hypothesis[i] = S[i]
-                    if new_hypothesis not in new_G:
-                        new_G.append(new_hypothesis)
-        G = new_G
+    print("\nInitial S:", specific_h)
+    print("Initial G:", general_h)
 
+    for i in range(len(target)):
+        instance = concepts[i]
+        output = target[i]
 
-print("S (Most Specific Hypothesis):")
-print(S)
+        print("\nExample", i+1, ":", instance, "->", output)
 
-print("\nG (Most General Hypotheses):")
-for g in G:
-    print(g)
+        if output == "yes":
+            # Generalize S
+            for j in range(len(specific_h)):
+                if instance[j] != specific_h[j]:
+                    specific_h[j] = "?"
+        else:
+            # Specialize G
+            new_rules = []
+            for j in range(len(specific_h)):
+                if instance[j] != specific_h[j]:
+                    rule = ["?" for _ in specific_h]
+                    rule[j] = specific_h[j]
+                    new_rules.append(rule)
+            general_h.extend(new_rules)
+
+        print("Updated S:", specific_h)
+        print("Updated G:", general_h)
+
+    cleaned_g = []
+    for g in general_h:
+        if g != ["?" for _ in specific_h] and g not in cleaned_g:
+            cleaned_g.append(g)
+
+    return specific_h, cleaned_g
+
+final_s, final_g = candidate_elimination(concepts, target)
+
+print("\nFinal Specific Hypothesis S:", final_s)
+print("Final General Hypothesis G:", final_g)
